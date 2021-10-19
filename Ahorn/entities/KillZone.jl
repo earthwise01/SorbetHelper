33ee1f34329ba42f5cd59278900ce8f4f523b7c1
Earthwise <home.earthwise@gmail.com>
2021-10-19 19:23:05 +1300

    Add rendering and selection behavior to the Ahorn plugin for Kill Zone.

--- Ahorn/entities/KillZone.jl
+++ Ahorn/entities/KillZone.jl
@@ -19,4 +19,20 @@ const placements = Ahorn.PlacementDict(
 Ahorn.minimumSize(entity::KillZone) = 8, 8
 Ahorn.resizable(entity::KillZone) = true, true
 
+function Ahorn.selection(entity::KillZone)
+    x, y = Ahorn.position(entity)
+
+    width = Int(get(entity.data, "width", 8))
+    height = Int(get(entity.data, "height", 8))
+ 
+    return Ahorn.Rectangle(x, y, width, height)
+end
+
+function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::KillZone, room::Maple.Room)
+    width = Int(get(entity.data, "width", 32))
+    height = Int(get(entity.data, "height", 32))
+    
+    Ahorn.drawRectangle(ctx, 0, 0, width, height, (1.0, 0.8, 0.85, 0.8), (0.0, 1.0, 1.0, 0.0))
+end
+
 end
