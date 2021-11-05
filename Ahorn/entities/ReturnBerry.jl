23600c9a2fd88b74ca7ab5bf734fa2d5a374ef8b
Earthwise <home.earthwise@gmail.com>
2021-11-05 19:47:46 +1300

    Remove debug prints and add comments in ReturnBerry.cs/.jl

--- Ahorn/entities/ReturnBerry.jl
+++ Ahorn/entities/ReturnBerry.jl
@@ -2,20 +2,21 @@ module SorbetHelperReturnBerry
 
 using ..Ahorn, Maple
 
+# Return Berry Ahorn plugin
+# Probably quite messy in places as it's mostly the Strawberry and Key Ahorn plugins smashed together.
+
 @mapdef Entity "SorbetHelper/ReturnBerry" ReturnBerry(
     x::Integer,
     y::Integer,
     winged::Bool=false,
 )
 
-println("adding placements")
 const placements = Ahorn.PlacementDict(
     "Strawberry (With Return) (Sorbet Helper)" => Ahorn.EntityPlacement(
         ReturnBerry,
         "point",
         Dict{String, Any}(),
         function(entity::ReturnBerry)
-            println("adding nodes")
             entity.data["nodes"] = [
                 (Int(entity.data["x"]) + 32, Int(entity.data["y"])),
                 (Int(entity.data["x"]) + 64, Int(entity.data["y"]))
@@ -37,22 +38,12 @@ const placements = Ahorn.PlacementDict(
     )
 )
 
-
-# function setSprite(entity::ReturnBerry)   
-#     if entity.data("winged") == true
-#         println("using winged sprite")
-#         sprite = "collectables/strawberry/wings01"
-#     else 
-#         println("using normal sprite")
-#         sprite = "collectables/strawberry/normal00"
-#     end
-# end
-
 const sprites = Dict{Bool, String}(
     (false) => "collectables/strawberry/normal00",
     (true) => "collectables/strawberry/wings01",
 )
 
+# set the node limits
 Ahorn.nodeLimits(entity::ReturnBerry) = length(get(entity.data, "nodes", [])) == 2 ? (2, 2) : (0, 0)
 
 function Ahorn.selection(entity::ReturnBerry)
@@ -87,15 +78,12 @@ function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::ReturnBe
     for node in nodes
         nx, ny = Int.(node)
 
-        println("drawing arrows")
         Ahorn.drawArrow(ctx, px, py, nx, ny, Ahorn.colors.selection_selected_fc, headLength=6)
         Ahorn.drawSprite(ctx, sprite, nx, ny)
         px, py = nx, ny
     end
 end
 
-println("running Ahorn.render")
-# Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::ReturnBerry, room::Maple.Room) = Ahorn.drawSprite(ctx, sprite, 0, 0)
 function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::ReturnBerry, room::Maple.Room)
     x, y = Ahorn.position(entity)
 
@@ -108,20 +96,3 @@ function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::ReturnBerry, roo
 end
 
 end
-# Ahorn.minimumSize(entity::KillZone) = 8, 8
-# Ahorn.resizable(entity::KillZone) = true, true
-
-# function Ahorn.selection(entity::ReturnBerry)
-#     x, y = Ahorn.position(entity)
-
-#     return Ahorn.Rectangle(x, y, 16, 16)
-# end
-
-# function Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::ReturnBerry, room::Maple.Room)
-#     # width = Int(get(entity.data, "width", 32))
-#     # height = Int(get(entity.data, "height", 32))
-    
-#     Ahorn.drawRectangle(ctx, 0, 0, 16, 16, (1.0, 0.8, 0.85, 0.8), (0.0, 1.0, 1.0, 0.0))
-# end
-
-#end
--- Entities/ReturnBerry.cs
+++ Entities/ReturnBerry.cs
@@ -19,15 +19,16 @@ namespace Celeste.Mod.SorbetHelper {
         public ReturnBerry(EntityData data, Vector2 offset, EntityID gid) : base(data, offset, gid) {
 			nodes = data.NodesOffset(offset);
 			Add(new PlayerCollider(OnPlayer));
+			// Creates a strawberry seed list with no strawberry seeds to effectively remove them.
 			if (data.Nodes != null && data.Nodes.Length != 0) {
 				Seeds = new List<StrawberrySeed>();
 			}
         }
 
         public new void OnPlayer(Player player) {
+			// Calls the OnPlayer function from Strawberry, then starts NodeRoutine if there are 2 or more nodes.
 			base.OnPlayer(player);
 			if (nodes != null && nodes.Length >= 2) {
-				Logger.Log("SorbetHelper", "Adding NodeRoutine!");
 				Add(new Coroutine(NodeRoutine(player)));
 				Collidable = false;
 			}
@@ -35,9 +36,9 @@ namespace Celeste.Mod.SorbetHelper {
         }
 		private IEnumerator NodeRoutine(Player player) {
 			yield return 0.3f;
+			// If the player is still alive, put them in the CassetteFly state
 			if (!player.Dead) {
 				Audio.Play("event:/game/general/cassette_bubblereturn", SceneAs<Level>().Camera.Position + new Vector2(160f, 90f));
-				Logger.Log("SorbetHelper", "Starting Cassete Fly!");
 				player.StartCassetteFly(nodes[1], nodes[0]);
 			}
 		}
