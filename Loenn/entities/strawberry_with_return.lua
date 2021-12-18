d7dabd63c5379d28866706b67f55a9c1513e676f
Earthwise <home.earthwise@gmail.com>
2021-12-18 22:40:28 +1300

    Create a Lönn plugin for Strawberry (With Return)

---
+++ Loenn/entities/strawberry_with_return.lua
@@ -0,0 +1,42 @@
+local return_berry = {}
+
+return_berry.name = "SorbetHelper/ReturnBerry"
+return_berry.depth = -100
+return_berry.nodeLineRenderType = "line"
+return_berry.nodeLimits = {2, 2}
+
+function return_berry.texture(room, entity)
+    local winged = entity.winged
+    if winged then
+        return "collectables/strawberry/wings01"
+    else
+        return "collectables/strawberry/normal00"
+    end
+end
+
+return_berry.placements = {
+    {
+        name = "normal",
+        placementType = "point",
+        data = {
+            winged = false,
+            nodes = {
+                {x = 0, y = 0},
+                {x = 0, y = 0}
+            }
+        }
+    },
+    {
+        name = "winged",
+        placementType = "point",
+        data = {
+            winged = true,
+            nodes = {
+                {x = 0, y = 0},
+                {x = 0, y = 0}
+            }
+        }
+    }
+}
+
+return return_berry
