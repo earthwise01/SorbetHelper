f8bb1b4054e75335ca226b1946a8421d43f5b02f
Earthwise <home.earthwise@gmail.com>
2021-12-19 13:29:17 +1300

    Change build.sh into package.sh and update it to also include the Loenn folder in the zip.

--- build.sh
+++
@@ -1,2 +0,0 @@
-dotnet build
-7z a SorbetHelper.zip bin/Debug/net452/ Ahorn/ everest.yaml LICENSE
---
+++ package.sh
@@ -0,0 +1,2 @@
+dotnet build
+7z a SorbetHelper.zip bin/Debug/net452/ Ahorn/ Loenn/ everest.yml LICENSE
