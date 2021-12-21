42faf0e77b6bdacd9bc2ac38efba885f24e79d89
Earthwise <home.earthwise@gmail.com>
2021-12-21 14:19:05 +1300

    Remove empty line in DashFallingBlock.cs

--- Entities/DashFallingBlock.cs
+++ Entities/DashFallingBlock.cs
@@ -56,8 +56,7 @@ namespace Celeste.Mod.SorbetHelper.Entities {
                 shaker.On = true;
                 bounce.Start();
                 isTriggered = true;
-
-				Audio.Play(impactSfx, base.Center);
+			    Audio.Play(impactSfx, base.Center);
                 return DashCollisionResults.Rebound;
             }
 
