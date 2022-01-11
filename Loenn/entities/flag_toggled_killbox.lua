cdc1737ab08de3826a8d0dd94bff8c90f79da5de
Earthwise <home.earthwise@gmail.com>
2022-01-11 23:03:07 +1300

    fix bug in flag toggled killbox loenn plugin

--- Loenn/entities/flag_toggled_killbox.lua
+++ Loenn/entities/flag_toggled_killbox.lua
@@ -1,17 +1,21 @@
+local utils = require("utils")
+
 local killbox = {}
 
 killbox.name = "SorbetHelper/FlagToggledKillbox"
-killbox.fillColor = {0.8, 0.4, 0.4, 0.8}
-killbox.borderColor = {0.0, 0.0, 0.0, 0.0}
+killbox.color = {0.8, 0.4, 0.4, 0.8}
 killbox.canResize = {true, false}
 killbox.placements = {
     name = "flag_toggled_killbox",
     data = {
         width = 8,
-        height = 32,
         flag = "",
         inverted = false
     }
 }
+
+function killbox.rectangle(room, entity)
+    return utils.rectangle(entity.x, entity.y, entity.width or 8, 32)
+end
 
 return killbox
