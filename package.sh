6e3baf0897dddf1b6cb66b11e2a77cd13e6f9c89
Earthwise <home.earthwise@gmail.com>
2022-01-13 14:37:04 +1300

    update package.sh to include graphics folder

--- package.sh
+++ package.sh
@@ -1,2 +1,3 @@
+rm SorbetHelper.zip
 dotnet build
-7z a SorbetHelper.zip bin/Debug/net452/ Ahorn/ Loenn/ everest.yml LICENSE
+7z a SorbetHelper.zip bin/Debug/net452/ Ahorn/ Loenn/ Graphics/ everest.yml
