dcc820219993412db069f4bb602b1977f0edea52
Earthwise <home.earthwise@gmail.com>
2022-06-07 18:03:00 +1200

    visual updates to dash falling block they'll now emit dust particles and shake when you dash into them

--- Entities/DashFallingBlock.cs
+++ Entities/DashFallingBlock.cs
@@ -18,9 +18,12 @@ namespace Celeste.Mod.SorbetHelper.Entities {
         public bool fallOnTouch;
         public bool fallOnStaticMover;
         public bool allowWavedash;
-        private bool dashCornerCorrection;
+        public bool dashCornerCorrection;
 
         public bool isTriggered;
+        public TileGrid tilegrid;
+        public Vector2 hitOffset;
+        public Vector2 scale = Vector2.One;
 
         public static void Load() {
             On.Celeste.FallingBlock.Sequence += onSequence;
@@ -37,6 +40,8 @@ namespace Celeste.Mod.SorbetHelper.Entities {
         }
 
         public DashFallingBlock(EntityData data, Vector2 offset) : base(data, offset) {
+            DynamicData fallingBlockData = new DynamicData(this);
+            Remove(fallingBlockData.Get<TileGrid>("tiles"));
             shakeSfx = data.Attr("shakeSfx", "event:/game/general/fallblock_shake");
             impactSfx = data.Attr("impactSfx", "event:/game/general/fallblock_impact");
             fallOnTouch = data.Bool("fallOnTouch", false);
@@ -44,6 +49,12 @@ namespace Celeste.Mod.SorbetHelper.Entities {
             allowWavedash = data.Bool("allowWavedash", false);
             dashCornerCorrection = data.Bool("dashCornerCorrection", false);
             base.Depth = data.Int("depth", base.Depth);
+            char tile = data.Char("tiletype", '3');
+            int newSeed = Calc.Random.Next();
+            Calc.PushRandom(newSeed);
+            Add(tilegrid = GFX.FGAutotiler.GenerateBox(tile, data.Width / 8, data.Height / 8).TileGrid);
+            Calc.PopRandom();
+            Add(new TileInterceptor(tilegrid, highPriority: false));
             OnDashCollide = OnDashCollision;
         }
 
@@ -56,6 +67,11 @@ namespace Celeste.Mod.SorbetHelper.Entities {
                 (Scene as Level).DirectionalShake(dir);
                 isTriggered = true;
                 Audio.Play(impactSfx, base.Center);
+                ActivateParticles(-dir);
+                scale = new Vector2(
+                    1f + Math.Abs(dir.Y) * 0.35f - Math.Abs(dir.X) * 0.35f,
+                    1f + Math.Abs(dir.X) * 0.35f - Math.Abs(dir.Y) * 0.35f);
+                hitOffset = dir * 5f;
                 if (allowWavedash && dir.Y == 1)
                     return DashCollisionResults.NormalCollision;
                 return DashCollisionResults.Rebound;
@@ -70,6 +86,44 @@ namespace Celeste.Mod.SorbetHelper.Entities {
                 base.OnStaticMoverTrigger(sm);
                 isTriggered = true;
             }
+        }
+
+        public override void OnShake(Vector2 amount) {
+            base.OnShake(amount);
+            tilegrid.Position += amount;
+        }
+
+        public override void Render() {
+            tilegrid.Alpha = 0f;
+
+            base.Render();
+
+            tilegrid.Alpha = 1f;
+
+            Vector2 position = Position + tilegrid.Position;
+
+            var clip = tilegrid.GetClippedRenderTiles();
+            //MTexture tile;
+
+            for (int tx = clip.Left; tx < clip.Right; tx++) {
+                for (int ty = clip.Top; ty < clip.Bottom; ty++) {
+                    Vector2 vec = new Vector2(position.X + tx * tilegrid.TileWidth, position.Y + ty * tilegrid.TileHeight) + (Vector2.One * 4f) + hitOffset;
+                    vec.X = Center.X + (vec.X - Center.X) * scale.X;
+                    vec.Y = Center.Y + (vec.Y - Center.Y) * scale.Y;
+
+                    //tile = tiles.Tiles[tx, ty];
+                    //if (tile != null)
+                    tilegrid.Tiles[tx, ty].DrawCentered(vec, Color.White, scale);
+                }
+            }
+        }
+
+        public override void Update() {
+            base.Update();
+            scale.X = Calc.Approach(scale.X, 1f, Engine.DeltaTime * 4f);
+            scale.Y = Calc.Approach(scale.Y, 1f, Engine.DeltaTime * 4f);
+            hitOffset.X = Calc.Approach(hitOffset.X, 0f, Engine.DeltaTime * 15f);
+            hitOffset.Y = Calc.Approach(hitOffset.Y, 0f, Engine.DeltaTime * 15f);
         }
 
         private static IEnumerator onSequence(On.Celeste.FallingBlock.orig_Sequence orig, FallingBlock self) {
@@ -104,5 +158,38 @@ namespace Celeste.Mod.SorbetHelper.Entities {
                 orig(self);
             }
         }
+
+        private void ActivateParticles(Vector2 dir) {
+			float direction;
+			Vector2 position;
+			Vector2 positionRange;
+			int num;
+			if (dir == Vector2.UnitX) {
+				direction = 0f;
+				position = base.CenterRight - Vector2.UnitX;
+				positionRange = Vector2.UnitY * (base.Height - 2f) * 0.5f;
+				num = (int)(base.Height / 8f) * 4;
+			}
+			else if (dir == -Vector2.UnitX) {
+				direction = (float)Math.PI;
+				position = base.CenterLeft + Vector2.UnitX;
+				positionRange = Vector2.UnitY * (base.Height - 2f) * 0.5f;
+				num = (int)(base.Height / 8f) * 4;
+			}
+			else if (dir == Vector2.UnitY) {
+				direction = (float)Math.PI / 2f;
+				position = base.BottomCenter - Vector2.UnitY;
+				positionRange = Vector2.UnitX * (base.Width - 2f) * 0.5f;
+				num = (int)(base.Width / 8f) * 4;
+			}
+			else {
+				direction = -(float)Math.PI / 2f;
+				position = base.TopCenter + Vector2.UnitY;
+				positionRange = Vector2.UnitX * (base.Width - 2f) * 0.5f;
+				num = (int)(base.Width / 8f) * 4;
+			}
+			num += 2;
+			SceneAs<Level>().Particles.Emit(P_LandDust, num, position, positionRange, direction);
+		}
     }
 }
