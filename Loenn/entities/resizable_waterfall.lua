0a2b3bca018a96580904df2f19f9d3c34590aaa1
Earthwise <home.earthwise@gmail.com>
2022-10-05 19:12:01 +1300

    lonn plugin polish

--- Loenn/entities/resizable_waterfall.lua
+++ Loenn/entities/resizable_waterfall.lua
@@ -14,8 +14,21 @@ resizableWaterfall.fieldInformation = {
     depth = {
         options = {-9999, -49900},
         editable = true
+    },
+    color = {
+        fieldType = "color",
+        allowXNAColors = false
     }
 }
+
+resizableWaterfall.fieldOrder = {
+    "x", "y", "width", "color", "depth", "lines", "ignoreSolids"
+}
+
+resizableWaterfall.ignoredFields = {
+    "height", "_id", "_name"
+}
+
 resizableWaterfall.placements = {
     {
         name = "normal",
@@ -40,15 +53,20 @@ resizableWaterfall.placements = {
 }
 
 function resizableWaterfall.sprite(room, entity)
-    -- cursed
-    local entityHackfix = entity
-    entityHackfix.height = waterfallHelper.getWaterfallHeight(room, entity)
+    -- height isn't ever supposed to be anything other than nil but it's probably bad practice to actively remove data anyway? meaning we back up the existing height value before doing a hackfix
+    local entityHeightHackfix = entity.height
+    -- temporarily change the height to make the waterfall Actually Work visually
+    entity.height = waterfallHelper.getWaterfallHeight(room, entity)
 
     local color = utils.getColor(entity.color)
 
     local fillColor = {color[1] * 0.3, color[2] * 0.3, color[3] * 0.3, 0.3}
     local borderColor = {color[1] * 0.8, color[2] * 0.8, color[3] * 0.8, 0.8}
-    return waterfallHelper.getBigWaterfallSprite(room, entityHackfix, fillColor, borderColor)
+    -- using the hackfixed height value from earlier, grab a correct big waterfall sprite
+    result = waterfallHelper.getBigWaterfallSprite(room, entity, fillColor, borderColor)
+    -- reset the height back to what it was before (it *should* always be nil but we're doing this instead of just setting it to nil due to the reason stated earlier)
+    entity.height = entityHeightHackfix
+    return result
 end
 
 function resizableWaterfall.rectangle(room, entity)
