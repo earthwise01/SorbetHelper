0764f6ca0bcd7aa176787a7e523994c38adf1a9e
Earthwise <home.earthwise@gmail.com>
2023-05-25 15:58:19 +1200

    it actually somewhat works now i guess???

--- Entities/WingedStrawberryDirectionController.cs
+++ Entities/WingedStrawberryDirectionController.cs
@@ -8,6 +8,7 @@ using Monocle;
 using MonoMod.Utils;
 using MonoMod.Cil;
 using Mono.Cecil.Cil;
+using Mono.Cecil;
 using MonoMod.RuntimeDetour;
 using Celeste.Mod;
 using Celeste.Mod.Entities;
@@ -15,39 +16,80 @@ using Celeste;
 
 namespace Celeste.Mod.SorbetHelper.Entities {
 
+    [CustomEntity("SorbetHelper/WingedStrawberryDirectionController")]
+    [Tracked]
     public class WingedStrawberryDirectionController : Entity {
 
         private static ILHook strawberryUpdateHook;
+        public enum Directions {Up, Down, Left, Right};
+        public Directions direction;
         public WingedStrawberryDirectionController(EntityData data, Vector2 offset) : base(data.Position + offset) {
+            direction = Directions.Left;
+        }
 
+        public override void Update()
+        {
+            base.Update();
         }
 
         public static void Load() {
-            //IL.Celeste.Strawberry.Update += modFlyDirection;
             strawberryUpdateHook = new ILHook(typeof(Strawberry).GetMethod("orig_Update"), modFlyDirection);
         }
 
         public static void Unload() {
-            //IL.Celeste.Strawberry.orig_Update += modFlyDirection;
             strawberryUpdateHook.Dispose();
         }
 
+        private static bool movesUpCheck(Strawberry self) {
+            WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
+            if (controller == null || controller.direction == Directions.Up) {
+                return true;
+            }
+            return false;
+        }
+
         private static void modFlyDirection(ILContext il) {
+            // this is probably an absolute mess but aaaaaaa it works i guess?????
             ILCursor cursor = new ILCursor(il);
 
-            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdfld<Strawberry>("flyingAway"))) {
-                Logger.Log("sdkjfasdkjhflaksdjfslakfdh", $"Applying hook at {cursor.Index} in CIL code for {cursor.Method.FullName}");
+            if (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdfld<Strawberry>("flyingAway"))) {
+                Logger.Log("sdksdkjhflaksdjfslakfdh", $"Applying hook at {cursor.Index} in CIL code for {cursor.Method.FullName}");
+                ILLabel l = cursor.DefineLabel();
 
-                //cursor.EmitDelegate<Func<float>>(determineDashLengthFactor);
-                //cursor.Emit(OpCodes.Mul);
-                if (cursor.TryGotoPrev(MoveType.Before, instr => instr.MatchAdd())) {
-                    cursor.Remove();
-                    cursor.Emit(OpCodes.Sub);
-                    return;
+                if (cursor.TryGotoPrev(MoveType.Before,
+                instr => instr.MatchLdarg(0),
+                instr => instr.MatchLdarg(0),
+                instr => instr.OpCode == OpCodes.Call && ((MethodReference) instr.Operand).Name.Contains("get_Y"))) {
+                    Logger.Log("sadksdkjhflaksdjfslakfdh", $"Applying hook at {cursor.Index} in CIL code for {cursor.Method.FullName}");
+
+                    cursor.Emit(OpCodes.Ldarg_0);
+                    cursor.EmitDelegate<Func<Strawberry, bool>>(movesUpCheck);
+                    cursor.Emit(OpCodes.Brfalse, l);
+                    cursor.GotoNext(MoveType.After, instr => instr.OpCode == OpCodes.Call && ((MethodReference) instr.Operand).Name.Contains("set_Y"))
+                    .MarkLabel(l);
+
+                    cursor.Emit(OpCodes.Ldarg_0);
+                    cursor.Emit(OpCodes.Ldarg_0);
+                    cursor.Emit(OpCodes.Ldfld, typeof(Strawberry).GetField("flapSpeed", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance));
+                    cursor.EmitDelegate<Action<Strawberry, float>>((self, flapSpeed) => {
+                        WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
+                        if (controller != null && controller.direction != Directions.Up) {
+                            switch (controller.direction) {
+                                case Directions.Down:
+                                    self.Y -= flapSpeed * Engine.DeltaTime;
+                                    break;
+                                case Directions.Left:
+                                    self.X += flapSpeed * Engine.DeltaTime;
+                                    break;
+                                case Directions.Right:
+                                    self.X -= flapSpeed * Engine.DeltaTime;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    });
                 }
-                //cursor.GotoPrev(MoveType.Before, instr => instr.MatchAdd());
-
-
             }
         }
     }    
