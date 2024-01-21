622f40113a27d8c7130b7262373a395b0adb4fd0
Earthwise <home.earthwise@gmail.com>
2024-01-22 02:40:29 +1300

    move emitdelegates into static methods apparently seems to improve performance a tiny tiny tiny bit this is probably done now i feel like at this point im just aimlessly trying to find *something* unfinished left that i missed

--- Entities/WingedStrawberryDirectionController.cs
+++ Entities/WingedStrawberryDirectionController.cs
@@ -21,16 +21,16 @@ namespace Celeste.Mod.SorbetHelper.Entities {
     public class WingedStrawberryDirectionController : Entity {
         private static ILHook strawberryUpdateHook;
 
-        private static Dictionary<string, Vector2> directionToVector = new Dictionary<string, Vector2>{
+        private static readonly Dictionary<string, Vector2> directionToVector = new Dictionary<string, Vector2>{
             {"up", new Vector2(0f, -1f)}, {"down", new Vector2(0f, 1f)}, {"left", new Vector2(-1f, 0f)}, {"right", new Vector2(1f, 0f)},
             {"upleft", new Vector2(-1f, -1f)}, {"upright", new Vector2(1f, -1f)}, {"downleft", new Vector2(-1f, 1f)}, {"downright", new Vector2(1f, 1f)}
         };
         public Vector2 direction;
 
-        public bool movesUp => direction.Y < 0f;
-        public bool movesDown => direction.Y > 0f;
-        public bool movesLeft => direction.X < 0f;
-        public bool movesRight => direction.X > 0f;
+        private bool movesUp => direction.Y < 0f;
+        private bool movesDown => direction.Y > 0f;
+        private bool movesLeft => direction.X < 0f;
+        private bool movesRight => direction.X > 0f;
 
         public WingedStrawberryDirectionController(EntityData data, Vector2 offset) : base(data.Position + offset) {
             direction = directionToVector[data.Attr("direction", "up").ToLower()];
@@ -61,23 +61,15 @@ namespace Celeste.Mod.SorbetHelper.Entities {
 
                 // skip over the vanilla code for moving the berry vertically if a controller exists
                 cursor.Emit(OpCodes.Ldarg_0);
-                cursor.EmitDelegate<Func<Strawberry, bool>>((self) => {
-                    return self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>() == null;
-                });
-                cursor.Emit(OpCodes.Brfalse, label);
+                cursor.EmitDelegate<Func<Strawberry, bool>>(controllerExists);
+                cursor.Emit(OpCodes.Brtrue, label);
                 cursor.GotoNext(MoveType.After, instr => instr.OpCode == OpCodes.Call && ((MethodReference)instr.Operand).Name.Contains("set_Y"))
                 .MarkLabel(label);
 
                 cursor.Emit(OpCodes.Ldarg_0);
                 cursor.Emit(OpCodes.Ldarg_0);
                 cursor.Emit(OpCodes.Ldfld, typeof(Strawberry).GetField("flapSpeed", BindingFlags.NonPublic | BindingFlags.Instance));
-                cursor.EmitDelegate<Action<Strawberry, float>>((self, flapSpeed) => {
-                    WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
-                    if (controller == null)
-                        return;
-
-                    self.Position += -controller.direction * flapSpeed * Engine.DeltaTime;
-                });
+                cursor.EmitDelegate<Action<Strawberry, float>>(moveStrawberry);
             }
 
             // inject downwards and horizontal out of room bounds checks.
@@ -90,28 +82,13 @@ namespace Celeste.Mod.SorbetHelper.Entities {
 
                 cursor.Emit(OpCodes.Ldarg_0);
                 // check if the berry flies upwards and if it doesn't, skip past the vanilla code for removing the berry once it flies above the room bounds
-                cursor.EmitDelegate<Func<Strawberry, bool>>((self) => {
-                    WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
-                    return controller == null || controller.movesUp;
-                });
+                cursor.EmitDelegate<Func<Strawberry, bool>>(strawberryFliesUp);
                 cursor.Emit(OpCodes.Brfalse, label2);
                 cursor.GotoNext(MoveType.After, instr => instr.OpCode == OpCodes.Call && ((MethodReference)instr.Operand).Name.Contains("RemoveSelf"))
                 .MarkLabel(label2);
 
                 cursor.Emit(OpCodes.Ldarg_0);
-                cursor.EmitDelegate<Action<Strawberry>>(self => {
-                    WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
-                    if (controller == null)
-                        return;
-
-                    // out of bounds checks are only done for the directions the berry flies towards bc otherwise berries that would fly into the room from offscreen will be removed as soon as they start flying
-                    if (controller.movesDown && self.Y > (float)(self.SceneAs<Level>().Bounds.Bottom + 16))
-                        self.RemoveSelf();
-                    if (controller.movesLeft && self.X < (float)(self.SceneAs<Level>().Bounds.Left - 24))
-                        self.RemoveSelf();
-                    if (controller.movesRight && self.X > (float)(self.SceneAs<Level>().Bounds.Right + 24))
-                        self.RemoveSelf();
-                });
+                cursor.EmitDelegate<Action<Strawberry>>(addExtraOutOfBoundsChecks);
             }
 
             // inject horizontal idle bounds checks alongside the vanilla vertical checks.
@@ -124,18 +101,49 @@ namespace Celeste.Mod.SorbetHelper.Entities {
                 cursor.Emit(OpCodes.Ldarg_0);
                 cursor.Emit(OpCodes.Ldarg_0);
                 cursor.Emit(OpCodes.Ldfld, typeof(Strawberry).GetField("start", BindingFlags.NonPublic | BindingFlags.Instance));
-                cursor.EmitDelegate<Action<Strawberry, Vector2>>((self, start) => {
-                    // only perform horizontal idle bounds checks if a controller exists
-                    WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
-                    if (controller == null)
-                        return;
-
-                    if (self.X < start.X - 5f)
-                        self.X = start.X - 5f;
-                    else if (self.Y > start.Y + 5f)
-                        self.X = start.X + 5f;
-                });
+                cursor.EmitDelegate<Action<Strawberry, Vector2>>(addHorizontalIdleBoundsChecks);
             }
+        }
+
+        private static bool controllerExists(Entity self) => self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>() != null;
+
+        private static void moveStrawberry(Entity self, float flapSpeed) {
+            WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
+            if (controller == null)
+                return;
+
+            self.Position += -controller.direction * flapSpeed * Engine.DeltaTime;
+        }
+
+        private static bool strawberryFliesUp(Entity self) {
+            WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
+            return controller == null || controller.movesUp;
+        }
+
+        private static void addExtraOutOfBoundsChecks(Entity self) {
+            WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
+            if (controller == null)
+                return;
+
+            // out of bounds checks are only done for the directions the berry flies towards bc otherwise berries that would fly into the room from offscreen will be removed as soon as they start flying
+            if (controller.movesDown && self.Y > (float)(self.SceneAs<Level>().Bounds.Bottom + 16))
+                self.RemoveSelf();
+            if (controller.movesLeft && self.X < (float)(self.SceneAs<Level>().Bounds.Left - 24))
+                self.RemoveSelf();
+            if (controller.movesRight && self.X > (float)(self.SceneAs<Level>().Bounds.Right + 24))
+                self.RemoveSelf();
+        }
+
+        private static void addHorizontalIdleBoundsChecks(Entity self, Vector2 start) {
+            // only perform horizontal idle bounds checks if a controller exists
+            WingedStrawberryDirectionController controller = self.Scene.Tracker.GetEntity<WingedStrawberryDirectionController>();
+            if (controller == null)
+                return;
+
+            if (self.X < start.X - 5f)
+                self.X = start.X - 5f;
+            else if (self.Y > start.Y + 5f)
+                self.X = start.X + 5f;
         }
     }
 }
