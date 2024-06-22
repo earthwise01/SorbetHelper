60391a36670ceabbd66f0de1a78e3acd7b6c5f48
Earthwise <home.earthwise@gmail.com>
2024-06-23 02:46:46 +1200

    made ignore solids actually affect the visuals of resizable waterfalls in loenn

--- Loenn/entities/resizable_waterfall.lua
+++ Loenn/entities/resizable_waterfall.lua
@@ -3,11 +3,11 @@ local roomStruct = require("structs.room")
 local xnaColors = require("consts.xna_colors")
 local utils = require("utils")
 local waterfallHelper = require("helpers.waterfalls")
+local connectedEntities = require("helpers.connected_entities")
 
 local resizableWaterfall = {}
 
 resizableWaterfall.name = "SorbetHelper/BigWaterfall"
-resizableWaterfall.depth = -9999
 resizableWaterfall.canResize = {true, false}
 resizableWaterfall.minimumSize = {16, 0}
 resizableWaterfall.fieldInformation = {
@@ -52,11 +52,57 @@ resizableWaterfall.placements = {
     }
 }
 
+-- normally loenn doesnt check for pandoras box water but i do here anyway bc itd look weird sometimes otherwise
+local function waterSearchPredicate(entity)
+    return entity._name == "water" or entity._name == "pandorasBox/coloredWater"
+end
+
+local function anyCollisions(rectangle, rectangles)
+    for _, rect in ipairs(rectangles) do
+        if utils.aabbCheck(rect, rectangle) then
+            return true
+        end
+    end
+
+    return false
+end
+
+-- stolen from loenn waterfallHelper but with the ability to ignore tiles
+local function getWaterfallHeight(room, entity, ignoreTiles)
+    local waterBlocks = utils.filter(waterSearchPredicate, room.entities)
+    local waterRectangles = connectedEntities.getEntityRectangles(waterBlocks)
+
+    local x, y = entity.x or 0, entity.y or 0
+    local tileX, tileY = math.floor(x / 8) + 1, math.floor(y / 8) + 1
+
+    local roomHeight = room.height
+    local wantedHeight = 8 - y % 8
+
+    local tileMatrix = room.tilesFg.matrix
+
+    while wantedHeight < roomHeight - y do
+        local rectangle = utils.rectangle(x, y + wantedHeight, 8, 8)
+
+        if anyCollisions(rectangle, waterRectangles) then
+            break
+        end
+
+        if not ignoreTiles and tileMatrix:get(tileX, tileY + 1, "0") ~= "0" then
+            break
+        end
+
+        wantedHeight += 8
+        tileY += 1
+    end
+
+    return wantedHeight
+end
+
 function resizableWaterfall.sprite(room, entity)
     -- height isn't ever supposed to be anything other than nil but it's probably bad practice to actively remove data anyway? meaning we back up the existing height value before doing a hackfix
     local entityHeightHackfix = entity.height
     -- temporarily change the height and layer to make the waterfall Actually Work visually
-    entity.height = waterfallHelper.getWaterfallHeight(room, entity)
+    entity.height = getWaterfallHeight(room, entity, entity.ignoreSolids)
     entity.layer = "FG"
 
     local color = utils.getColor(entity.color)
@@ -73,9 +119,17 @@ end
 
 function resizableWaterfall.rectangle(room, entity)
     local x, y = entity.x or 0, entity.y or 0
-    local height = waterfallHelper.getWaterfallHeight(room, entity)
+    local height = getWaterfallHeight(room, entity, entity.ignoreSolids)
 
     return utils.rectangle(x, y, entity.width, height)
+end
+
+function resizableWaterfall.depth(room, entity)
+    if entity.ignoreSolids then
+        return -49900
+    end
+
+    return -9999
 end
 
 return resizableWaterfall
