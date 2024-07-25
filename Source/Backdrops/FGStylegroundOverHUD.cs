904883ae391f2c44f40d02a77425476ccc67e4d7
Earthwise <home.earthwise@gmail.com>
2024-07-26 03:19:20 +1200

    draw stylegrounds directly to the screen rather than going through a buffer first by default fixes opaque stylegrounds being completely broken looking when combined with transparent ones, potentially adds issues with certain stylegrounds and also allows some effects to draw to subpixel positions

--- Source/Backdrops/FGStylegroundOverHUD.cs
+++ Source/Backdrops/FGStylegroundOverHUD.cs
@@ -17,7 +17,13 @@ namespace Celeste.Mod.SorbetHelper.Backdrops {
         private static VirtualRenderTarget Buffer;
         private static FGStylegroundOverHUDRenderer Instance;
 
+        // fixes some extremely broken rendering with transparent stylegrounds but might produce other weird results maybe possibly
+        // or break if someone messes with switching render targets midrender and doesnt set it back correctly (displacement wrapper flashbacks i still need to make that better there huh right asdfahks)
+        // if i dont find anything completely awful i might make this the behavior by default but for now im leaving it as a toggle just in case
+        private static bool fixWeirdAlphaRenderingIssuesButMaybeCauseOtherIssuesIdk = true;
+
         private void ConsumeStylegrounds(Level level) {
+            fixWeirdAlphaRenderingIssuesButMaybeCauseOtherIssuesIdk = true;
             List<Backdrop> origBackdrops = level.Foreground.Backdrops;
 
             // i dont know why the fk i need to do this but for some bizarre reason it literally doesnt work if i iterate through the list forwards catplush
@@ -30,6 +36,8 @@ namespace Celeste.Mod.SorbetHelper.Backdrops {
                         Backdrops.Insert(0, backdrop);
                         backdrop.Renderer = backdropRenderer;
                         origBackdrops.RemoveAt(i);
+                    } else if (tag == "sorbetHelper_useOldDrawAboveHudBehavior") {
+                        fixWeirdAlphaRenderingIssuesButMaybeCauseOtherIssuesIdk = false;
                     }
                 }
             }
@@ -42,6 +50,11 @@ namespace Celeste.Mod.SorbetHelper.Backdrops {
                 backdropRenderer.Update(level);
 
             backdropRenderer.BeforeRender(level);
+
+            if (fixWeirdAlphaRenderingIssuesButMaybeCauseOtherIssuesIdk)
+                return;
+
+            // old behavior
 
             Buffer ??= VirtualContent.CreateRenderTarget("fgstylegrounds_above_hud_buffer", 320, 180);
 
@@ -63,7 +76,7 @@ namespace Celeste.Mod.SorbetHelper.Backdrops {
         public override void Render(Scene scene) {
             Level level = scene as Level;
 
-            if (Backdrops.Count <= 0 || Buffer is null)
+            if (Backdrops.Count <= 0)
                 return;
 
             // copied from vanilla Level.Render when drawing the level to the screen
@@ -74,6 +87,21 @@ namespace Celeste.Mod.SorbetHelper.Backdrops {
             Vector2 vector3 = (level.ZoomTarget != 1f) ? ((level.ZoomFocusPoint - vector2 / 2f) / (vector - vector2) * vector) : Vector2.Zero;
             float scale = level.Zoom * ((320f - level.ScreenPadding * 2f) / 320f);
             Vector2 vector4 = new Vector2(level.ScreenPadding, level.ScreenPadding * 0.5625f);
+
+            if (fixWeirdAlphaRenderingIssuesButMaybeCauseOtherIssuesIdk) {
+                matrix = Matrix.CreateTranslation(new Vector3(vector4, 0f)) * Matrix.CreateTranslation(new Vector3(-vector3, 0f)) * Matrix.CreateScale(scale) * Matrix.CreateTranslation(new Vector3(vector3, 0f)) * matrix;
+                backdropRenderer.Matrix = matrix;
+
+                backdropRenderer.Render(level);
+
+                return;
+            }
+
+            // old behavior
+
+            if (Buffer is null)
+                return;
+
             if (SaveData.Instance.Assists.MirrorMode) {
                 vector4.X = 0f - vector4.X;
                 vector3.X = 160f - (vector3.X - 160f);
