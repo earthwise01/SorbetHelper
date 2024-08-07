8b175185659d81ce47f137f490cd87fa99443a13
Earthwise <home.earthwise@gmail.com>
2024-08-08 03:57:55 +1200

    basic fixes/visuals for touch move blocks just a ripoff of move blocks will probably try to make at least a bit more distinct still

--- Source/Entities/TouchGateBlock.cs
+++ Source/Entities/TouchGateBlock.cs
@@ -1,45 +1,65 @@
-// will try and re-add later but disabled for now
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Monocle;
+using Celeste.Mod.Entities;
 
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using System.Reflection;
-// using Microsoft.Xna.Framework;
-// using Microsoft.Xna.Framework.Graphics;
-// using Monocle;
-// using MonoMod.Utils;
-// using Celeste.Mod.Entities;
+namespace Celeste.Mod.SorbetHelper.Entities {
 
-// namespace Celeste.Mod.SorbetHelper.Entities {
+    [CustomEntity("SorbetHelper/TouchGateBlock")]
+    [Tracked]
+    public class TouchGateBlock : GateBlock {
 
-//     [CustomEntity("SorbetHelper/TouchGateBlock")]
-//     [Tracked]
-//     public class TouchGateBlock : GateBlock {
+        private readonly MTexture mainTexture;
 
-//         private bool moveOnGrab;
-//         private bool moveOnStaticMover;
+        private readonly bool moveOnGrab;
+        private readonly bool moveOnStaticMover;
 
-//         public TouchGateBlock(EntityData data, Vector2 offset) : base(data, offset) {
-//             moveOnGrab = data.Bool("moveOnGrab", true);
-//             moveOnStaticMover = data.Bool("moveOnStaticMoverInteract", false);
-//         }
+        public TouchGateBlock(EntityData data, Vector2 offset) : base(data, offset) {
+            moveOnGrab = data.Bool("moveOnGrab", true);
+            moveOnStaticMover = data.Bool("moveOnStaticMoverInteract", false);
 
-//         public override void OnStaticMoverTrigger(StaticMover sm) {
-//             if (!Triggered && moveOnStaticMover) {
-//                 Triggered = true;
-//                 TriggerLinked();
-//                 Audio.Play("event:/game/general/fallblock_shake", Position);
-//             }
-//         }
+            string blockSprite = data.Attr("blockSprite", "moveBlock/base");
+            mainTexture = GFX.Game[$"objects/{blockSprite}"];
+        }
 
-//         public override bool TriggerCheck() {
-//             if (!Triggered && (moveOnGrab && HasPlayerRider()) || (!moveOnGrab && HasPlayerOnTop())) {
-//                 Triggered = true;
-//                 TriggerLinked();
-//                 Audio.Play("event:/game/general/fallblock_shake", Position);
-//             }
-//             return base.TriggerCheck();
-//         }
+        public override void OnStaticMoverTrigger(StaticMover sm) {
+            if (!Triggered && moveOnStaticMover) {
+                Activate();
+                Audio.Play("event:/game/general/fallblock_shake", Position);
+                Audio.Play("event:/game/04_cliffside/arrowblock_activate", Center);
+            }
+        }
 
-//     }
-// }
+        public override void Update() {
+            if (!Triggered && ((moveOnGrab && HasPlayerRider()) || (!moveOnGrab && HasPlayerOnTop()))) {
+                Activate();
+                Audio.Play("event:/game/general/fallblock_shake", Position);
+                Audio.Play("event:/game/04_cliffside/arrowblock_activate", Center);
+            }
+
+            base.Update();
+        }
+
+        public override void Render() {
+            if (!VisibleOnCamera)
+                return;
+
+            // outline
+            Vector2 scaledTopLeft = Center + Offset - (new Vector2(Collider.Width / 2f, Collider.Height / 2f) * Scale);
+            float scaledWidth = Collider.Width * Scale.X;
+            float scaledHeight = Collider.Height * Scale.Y;
+            Draw.Rect(scaledTopLeft - Vector2.One, scaledWidth + 2, scaledHeight + 2, Color.Black);
+
+            // main block
+            Draw.Rect(scaledTopLeft + Vector2.One, scaledWidth - 2, scaledHeight - 2, currentColor);
+            DrawNineSlice(mainTexture, Color.White);
+
+            // render icon
+            base.Render();
+        }
+    }
+}
