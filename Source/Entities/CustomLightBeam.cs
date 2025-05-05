58e6f0483ffda3bce30d0a69b6984c3de47b22ae
earthwise <home.earthwise@gmail.com>
2025-05-05 20:34:36 +1200

    tidy up and fix alpha issue with custom lightbeams (untested but shouldd work please)

--- Source/Entities/CustomLightBeam.cs
+++ Source/Entities/CustomLightBeam.cs
@@ -12,50 +12,47 @@ namespace Celeste.Mod.SorbetHelper.Entities {
 
     [CustomEntity("SorbetHelper/CustomLightbeam")]
     public class CustomLightBeam : Entity {
-        public string flag;
-        public bool inverted;
+        private readonly string flag;
+        private readonly bool invertFlag;
         private readonly float flagFadeTime;
 
-        public float rotation;
+        private readonly float rotation;
 
-        private readonly bool fadeWhenNear = true;
-        private readonly bool fadeOnTransition = true;
+        private readonly bool fadeWhenNear;
+        private readonly bool fadeOnTransition;
+        private readonly bool noParticles;
+
+        private Color color;
 
         private readonly bool rainbow;
-
-        // used only when either rainbow == true && rainbowSingleColor == true or rainbow == false, otherwise ignored in favor of directly calling GetHue.
-        // probably slightly messy but results in less unnecessary changes to the variable.
-        public Color color = new Color(0.8f, 1f, 1f);
+        private const int rainbowSegmentSize = 4;
 
         private readonly bool useCustomRainbowColors;
 
-        public List<Color> rainbowColors = new List<Color>();
+        private readonly List<Color> rainbowColors = [];
         private readonly float rainbowGradientSize;
         private readonly float rainbowGradientSpeed;
         private readonly bool rainbowLoopColors;
-        private Vector2 rainbowCenter;
+        private readonly Vector2 rainbowCenter;
         private readonly bool rainbowSingleColor;
 
-        private const int rainbowSegmentSize = 4;
-
-        private readonly bool noParticles;
-
-        private readonly float Scroll;
-        private Vector2 ScrollAnchor;
+        private readonly float scroll;
+        private readonly Vector2 scrollAnchor;
         private Vector2 RenderPosition {
             get {
                 // edge case so normal lightbeams dont need to bother with anything
-                if (Scroll == 1f)
+                if (scroll == 1f)
                     return Position;
 
                 // hopefully i   mathed right and this actually fully does what i think it does
                 var cam = (Scene as Level).Camera.GetCenter();
-                return cam + (Position - ScrollAnchor * (1f - Scroll) - cam * Scroll);
+                return cam + (Position - scrollAnchor * (1f - scroll) - cam * scroll);
             }
         }
 
-        private float baseAlpha = 1;
-        private float flagAlpha = 1;
+        private readonly float baseAlpha = 1f;
+        private float distanceAlpha = 1f;
+        private float flagAlpha = 1f;
         private float alpha;
 
         private readonly int lightWidth;
@@ -64,15 +61,9 @@ namespace Celeste.Mod.SorbetHelper.Entities {
         private float timer = Calc.Random.NextFloat(1000f);
 
         private readonly MTexture beamTexture;
-
-        private static bool useRenderTarget = false; // originally added this because i was worried abt performance but  honestly i dont know if its rly needed or might be worse
-                                                     //  not fully removing in case it was actually helping n i need to revert so an unchanging "toggle" is good enough
-        private VirtualRenderTarget lightbeamTarget;
-        private Matrix targetMatrix;
         private readonly float offset;
 
         private readonly float rectangleTop, rectangleBottom, rectangleLeft, rectangleRight;
-        private bool wasVisibleOnCamera;
         private const int visibilityPadding = 16;
 
         public CustomLightBeam(EntityData data, Vector2 offset) : base(data.Position + offset) {
@@ -84,7 +75,7 @@ namespace Celeste.Mod.SorbetHelper.Entities {
             base.Depth = data.Int("depth", -9998);
 
             flag = data.Attr("flag");
-            inverted = data.Bool("inverted", false);
+            invertFlag = data.Bool("inverted", false);
             flagFadeTime = Math.Max(data.Float("flagFadeTime", 0.25f), 0f);
             rotation = data.Float("rotation", 0f) * ((float)Math.PI / 180f);
 
@@ -92,18 +83,18 @@ namespace Celeste.Mod.SorbetHelper.Entities {
             fadeOnTransition = data.Bool("fadeOnTransition", true);
 
             // eh - now less but   eh,
-            Scroll = data.Float("scroll", 1f);
+            scroll = data.Float("scroll", 1f);
             if (data.Nodes?.Length >= 1)
-                ScrollAnchor = data.Nodes[0] + offset;
+                scrollAnchor = data.Nodes[0] + offset;
             else
-                ScrollAnchor = Position;
+                scrollAnchor = Position;
             //ScrollAnchor = Position + new Vector2(data.Float("scrollAnchorX"), data.Float("scrollAnchorY"));
 
-            float alpha = Math.Clamp(data.Float("alpha", 1f), 0f, 1f);
+            baseAlpha = Math.Clamp(data.Float("alpha", 1f), 0f, 1f);
 
             beamTexture = GFX.Game[data.Attr("texture", "util/lightbeam")];
             noParticles = data.Bool("noParticles", false);
-            color = Util.HexToColorWithAlphaNonPremult(data.Attr("color", "CCFFFF")) * alpha;
+            color = Util.HexToColorWithAlphaNonPremult(data.Attr("color", "CCFFFF"));
             rainbow = data.Bool("rainbow", false);
             useCustomRainbowColors = data.Bool("useCustomRainbowColors", false);
 
@@ -115,125 +106,90 @@ namespace Celeste.Mod.SorbetHelper.Entities {
                 rainbowSingleColor = data.Bool("singleColor", false);
 
                 string[] colorsAsStrings = data.Attr("colors", "89E5AE,88E0E0,87A9DD,9887DB,D088E2").Split(',');
-                for (int i = 0; i < colorsAsStrings.Length; i++) {
-                    rainbowColors.Add(Util.HexToColorWithAlphaNonPremult(colorsAsStrings[i]) * alpha);
-                }
-                if (rainbowLoopColors) {
+                for (int i = 0; i < colorsAsStrings.Length; i++)
+                    rainbowColors.Add(Util.HexToColorWithAlphaNonPremult(colorsAsStrings[i]));
+                if (rainbowLoopColors)
                     rainbowColors.Add(rainbowColors[0]);
-                }
             }
 
             // offscreen culling stuff
             // kinda janky i think but it works
-            Vector2 baseCornerA = -Calc.AngleToVector(rotation, 1f) * (lightWidth / 2f); // would be top left with a rotation of 0f
-            Vector2 baseCornerB = Calc.AngleToVector(rotation, 1f) * (lightWidth / 2); // would be top right with a rotation of 0f
-            Vector2 edgeCornerA = baseCornerA + Calc.AngleToVector(rotation + (float)Math.PI / 2f, 1f) * lightLength; // would be bottom left with a rotation of 0f
-            Vector2 edgeCornerB = baseCornerB + Calc.AngleToVector(rotation + (float)Math.PI / 2f, 1f) * lightLength; // would be bottom right with a rotation of 0f
+            var baseCornerA = -Calc.AngleToVector(rotation, 1f) * (lightWidth / 2f); // would be top left with a rotation of 0f
+            var baseCornerB = Calc.AngleToVector(rotation, 1f) * (lightWidth / 2); // would be top right with a rotation of 0f
+            var edgeCornerA = baseCornerA + Calc.AngleToVector(rotation + (float)Math.PI / 2f, 1f) * lightLength; // would be bottom left with a rotation of 0f
+            var edgeCornerB = baseCornerB + Calc.AngleToVector(rotation + (float)Math.PI / 2f, 1f) * lightLength; // would be bottom right with a rotation of 0f
 
             rectangleTop = Math.Min(Math.Min(baseCornerA.Y, baseCornerB.Y), Math.Min(edgeCornerA.Y, edgeCornerB.Y));
             rectangleBottom = Math.Max(Math.Max(baseCornerA.Y, baseCornerB.Y), Math.Max(edgeCornerA.Y, edgeCornerB.Y));
             rectangleLeft = Math.Min(Math.Min(baseCornerA.X, baseCornerB.X), Math.Min(edgeCornerA.X, edgeCornerB.X));
             rectangleRight = Math.Max(Math.Max(baseCornerA.X, baseCornerB.X), Math.Max(edgeCornerA.X, edgeCornerB.X));
-
-            if (useRenderTarget)
-                Add(new BeforeRenderHook(BeforeRender));
         }
 
         public override void Awake(Scene scene) {
             base.Awake(scene);
-            Level level = base.Scene as Level;
+            var level = Scene as Level;
 
-            if (!string.IsNullOrEmpty(flag) && !level.Session.GetFlag(flag, inverted)) {
+            if (!string.IsNullOrEmpty(flag) && !level.Session.GetFlag(flag, invertFlag))
                 flagAlpha = 0f;
-            } else {
+            else
                 flagAlpha = 1f;
-            }
 
-            if (level.Transitioning && fadeOnTransition) {
-                baseAlpha = 0f;
-            }
+            if (level.Transitioning && fadeOnTransition)
+                distanceAlpha = 0f;
 
-            // initialize render target and matrix
-            if (!useRenderTarget)
-                return;
-
-            // whY did i do this   huhh?? what was i thinking
-            lightbeamTarget ??= VirtualContent.CreateRenderTarget("sorbet-custom-lightbeam", (int)(rectangleRight - rectangleLeft + visibilityPadding), (int)(rectangleBottom - rectangleTop + visibilityPadding));
-            Vector2 offset = Calc.AngleToVector(rotation - (float)Math.PI / 2f, lightLength) / 2f;
-            targetMatrix = Matrix.CreateTranslation(lightbeamTarget.Width / 2f, lightbeamTarget.Height / 2f, 0f) * Matrix.CreateTranslation(offset.X, offset.Y, 0f);
-            RenderToTarget();
-        }
-
-        // clean up render target
-        public override void Removed(Scene scene) {
-            Dispose();
-            base.Removed(scene);
-        }
-
-        public override void SceneEnd(Scene scene) {
-            Dispose();
-            base.SceneEnd(scene);
-        }
-
-        public void Dispose() {
-            lightbeamTarget?.Dispose();
-            lightbeamTarget = null;
+            alpha = baseAlpha * distanceAlpha * flagAlpha;
         }
 
         public override void Update() {
+            base.Update();
+
             timer += Engine.DeltaTime;
-            Level level = Scene as Level;
-            Player entity = Scene.Tracker.GetEntity<Player>();
+            var level = Scene as Level;
+            var player = Scene.Tracker.GetEntity<Player>();
 
             var pos = Position;
             Position = RenderPosition;
 
-            wasVisibleOnCamera = Visible;
             Visible = InView(level.Camera);
 
             // vanilla lightbeam fading
-            if (entity != null) {
-                float target = 1f;
+            if (player != null) {
+                float targetAlpha = 1f;
                 if (fadeWhenNear) {
-                    Vector2 vector = Calc.AngleToVector(rotation + (float)Math.PI / 2f, 1f);
-                    Vector2 vector2 = Calc.ClosestPointOnLine(Position, Position + vector * 10000f, entity.Center);
-                    target = Math.Min(1f, Math.Max(0f, (vector2 - Position).Length() - 8f) / lightLength);
-                    if ((vector2 - entity.Center).Length() > lightWidth / 2f) {
-                        target = 1f;
-                    }
+                    var direction = Calc.AngleToVector(rotation + (float)Math.PI / 2f, 1f);
+                    var playerDistancePoint = Calc.ClosestPointOnLine(Position, Position + direction * 10000f, player.Center);
+
+                    targetAlpha = Math.Min(1f, Math.Max(0f, (playerDistancePoint - Position).Length() - 8f) / lightLength);
+                    if ((playerDistancePoint - player.Center).Length() > lightWidth / 2f)
+                        targetAlpha = 1f;
                 }
-                if (level.Transitioning && fadeOnTransition) {
-                    target = 0f;
-                }
-                baseAlpha = Calc.Approach(baseAlpha, target, Engine.DeltaTime * 4f);
+
+                if (level.Transitioning && fadeOnTransition)
+                    targetAlpha = 0f;
+
+                distanceAlpha = Calc.Approach(distanceAlpha, targetAlpha, Engine.DeltaTime * 4f);
             }
 
             // fade flagAlpha towards either 0f or 1f depending on the flag state.
             if (!string.IsNullOrEmpty(flag)) {
-                float flagTarget;
-                if (!level.Session.GetFlag(flag, inverted)) {
-                    flagTarget = 0f;
-                } else {
-                    flagTarget = 1f;
-                }
-                flagAlpha = Calc.Approach(flagAlpha, flagTarget, Engine.DeltaTime / flagFadeTime);
+                float targetAlpha = level.Session.GetFlag(flag, invertFlag) ? 1f : 0f;
+                flagAlpha = Calc.Approach(flagAlpha, targetAlpha, Engine.DeltaTime / flagFadeTime);
             }
 
-            // multiply baseAlpha and flagAlpha together to get the actual alpha of the lightbeam.
-            alpha = baseAlpha * flagAlpha;
+            // multiply baseAlpha, distanceAlpha, and flagAlpha together to get the actual alpha of the lightbeam.
+            alpha = baseAlpha * distanceAlpha * flagAlpha;
 
             // emit particles
             if (Visible && !noParticles && alpha >= 0.5f && level.OnInterval(0.8f, offset)) {
-                Vector2 vector3 = Calc.AngleToVector(rotation + (float)Math.PI / 2f, 1f);
-                Vector2 position = Position - vector3 * 4f;
-                float num = Calc.Random.Next(lightWidth - 4) + 2 - lightWidth / 2;
-                position += num * vector3.Perpendicular();
+                var direction = Calc.AngleToVector(rotation + (float)Math.PI / 2f, 1f);
+                var particlePos = Position - direction * 4f;
+                float emitX = Calc.Random.Next(lightWidth - 4) + 2 - lightWidth / 2;
+                particlePos += emitX * direction.Perpendicular();
                 // if rainbow is enabled and rainbowSingleColor is disabled, call GetHue for the particle's color, otherwise use the color variable.
+                var particleColor = ((rainbow && !rainbowSingleColor) ? GetHue(particlePos) : color) * alpha;
                 // doesn't track properly with parallax but idk
-                level.Particles.Emit(LightBeam.P_Glow, position, (rainbow && !rainbowSingleColor) ? GetHue(position) : color, rotation + (float)Math.PI / 2f);
+                level.Particles.Emit(LightBeam.P_Glow, particlePos, particleColor, rotation + (float)Math.PI / 2f);
             }
-
-            base.Update();
 
             Position = pos;
         }
@@ -253,56 +209,21 @@ namespace Celeste.Mod.SorbetHelper.Entities {
             Draw.HollowRect(rectangleLeft - visibilityPadding / 2, rectangleTop - visibilityPadding / 2, rectangleRight - rectangleLeft + visibilityPadding, rectangleBottom - rectangleTop + visibilityPadding, Color.Yellow * 0.5f);
         } */
 
-        public void BeforeRender() {
-            // only update graphics every 3 frames when on camera or when the lightbeam comes on screen
-            if ((!Visible || !Scene.OnRawInterval(0.05f, offset)) && Visible == wasVisibleOnCamera)
+        public override void Render() {
+            base.Render();
+
+            if (alpha <= 0f)
                 return;
 
-            RenderToTarget();
-        }
-
-        public override void Render() {
-            if (alpha > 0f) {
-                var pos = Position;
-                Position = RenderPosition;
-
-                if (useRenderTarget)
-                    Draw.SpriteBatch.Draw(lightbeamTarget, Position + new Vector2(rectangleLeft - visibilityPadding / 2, rectangleTop - visibilityPadding / 2), null, Color.White * alpha, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-                else
-                    RenderLightbeam();
-
-                Position = pos;
-            }
-        }
-
-        private void RenderToTarget() {
-            Engine.Graphics.GraphicsDevice.SetRenderTarget(lightbeamTarget);
-            Engine.Graphics.GraphicsDevice.Clear(Color.Transparent);
-
             var pos = Position;
-            var renderPos = RenderPosition;
+            Position = RenderPosition;
 
-            var matrix = targetMatrix * Matrix.CreateTranslation(new Vector3(-renderPos.X, -renderPos.Y, 0f));
-            Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap, DepthStencilState.None, RasterizerState.CullNone, null, matrix);
-
-            Position = renderPos;
-            var a = alpha;
-            alpha = 1f;
-
-            RenderLightbeam();
-
-            alpha = a;
-            Position = pos;
-
-            Draw.SpriteBatch.End();
-        }
-
-        private void RenderLightbeam() {
             // update the hue of the rainbow lightbeam when rainbowSingleColor is enabled, otherwise GetHue is called directly whenever a color is needed.
             if (rainbow && rainbowSingleColor)
                 color = GetHue(Position);
 
             // render the lightbeam
+            // base
             if (rainbow && !rainbowSingleColor) {
                 // draw the base in 4px segments to make a gradient effect
                 for (int i = 0; i < lightWidth; i += rainbowSegmentSize) {
@@ -311,6 +232,8 @@ namespace Celeste.Mod.SorbetHelper.Entities {
             } else {
                 DrawBeam(0f, lightWidth, lightLength - 4 + (float)Math.Sin(timer * 2f) * 4f, 0.4f);
             }
+
+            // beams
             for (int i = 0; i < lightWidth; i += 4) {
                 float num = timer + i * 0.6f;
                 float num2 = 4f + (float)Math.Sin(num * 0.5f + 1.2f) * 4f;
@@ -319,12 +242,14 @@ namespace Celeste.Mod.SorbetHelper.Entities {
                 float a = 0.6f + (float)Math.Sin(num + 0.8f) * 0.3f;
                 DrawBeam(offset, num2, length, a);
             }
+
+            Position = pos;
         }
 
         private void DrawBeam(float offset, float width, float length, float a) {
             float beamRotation = rotation + (float)Math.PI / 2f;
             // if rainbow is enabled and rainbowSingleColor is disabled, call GetHue for the beam's color, otherwise use the color variable.
-            Color beamColor = ((rainbow && !rainbowSingleColor) ? GetHue(Position + Calc.AngleToVector(rotation, 1f) * offset) : color) * a * alpha;
+            var beamColor = ((rainbow && !rainbowSingleColor) ? GetHue(Position + Calc.AngleToVector(rotation, 1f) * offset) : color) * a * alpha;
 
             if (width >= 1f) {
                 beamTexture.Draw(Position + Calc.AngleToVector(rotation, 1f) * offset, new Vector2(0f, 0.5f), beamColor, new Vector2(1f / beamTexture.Width * length, width), beamRotation);
