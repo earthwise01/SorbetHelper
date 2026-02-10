56c94b4eef8764108e1628362bafd03af49d1dc3
earthwise <home.earthwise@gmail.com>
2026-02-10 22:29:19 +1300

    whatttt blows myself up

--- Source/Entities/ReturnBubbleBehaviorController.cs
+++ Source/Entities/ReturnBubbleBehaviorController.cs
@@ -42,8 +42,9 @@ public class ReturnBubbleBehaviorController : Entity {
 
     private readonly ReturnBubbleBehavior behavior;
 
-    public ReturnBubbleBehaviorController(EntityData data, Vector2 _) : base() {
+    public ReturnBubbleBehaviorController(EntityData data, Vector2 offset) : base() {
         behavior = ReturnBubbleBehavior.FromData(data);
+        LoadLazyHooksIfNeeded(behavior);
     }
 
     private static ReturnBubbleBehavior GetReturnBubbleBehavior(Scene scene)
@@ -67,7 +68,6 @@ public class ReturnBubbleBehaviorController : Entity {
         On.Celeste.Player.CassetteFlyEnd += On_CassetteFlyEnd;
         On.Celeste.Audio.CreateInstance += On_Audio_CreateInstance;
         On.Celeste.Player.OnSquish += On_OnSquish;
-        IL.Monocle.Collide.Check_Entity_Entity += IL_Collide_Check;
     }
 
     internal static void Unload() {
@@ -75,7 +75,24 @@ public class ReturnBubbleBehaviorController : Entity {
         On.Celeste.Player.CassetteFlyEnd -= On_CassetteFlyEnd;
         On.Celeste.Audio.CreateInstance -= On_Audio_CreateInstance;
         On.Celeste.Player.OnSquish -= On_OnSquish;
-        IL.Monocle.Collide.Check_Entity_Entity -= IL_Collide_Check;
+
+        UnloadLazyHooksIfNeeded();
+    }
+
+    private static bool collideCheckHookLoaded = false;
+
+    private static void LoadLazyHooksIfNeeded(ReturnBubbleBehavior behavior) {
+        if (!collideCheckHookLoaded && behavior.CollisionMode is ReturnBubbleBehavior.CollisionModes.NoCollide or ReturnBubbleBehavior.CollisionModes.TriggersOnly) {
+            IL.Monocle.Collide.Check_Entity_Entity += IL_Collide_Check;
+            collideCheckHookLoaded = true;
+        }
+    }
+
+    private static void UnloadLazyHooksIfNeeded() {
+        if (collideCheckHookLoaded) {
+            IL.Monocle.Collide.Check_Entity_Entity -= IL_Collide_Check;
+            collideCheckHookLoaded = false;
+        }
     }
 
     private static IEnumerator On_CassetteFlyCoroutine(On.Celeste.Player.orig_CassetteFlyCoroutine orig, Player self) {
@@ -96,10 +113,11 @@ public class ReturnBubbleBehaviorController : Entity {
         self.level.CanRetry = false;
         self.level.FormationBackdrop.Display = true;
         self.level.FormationBackdrop.Alpha = 0.5f;
-        self.Sprite.Scale = new Vector2(1.25f); // madeline scales back to 1x automatically so this just makes a little bounce effect
+        self.Sprite.Scale = new Vector2(1.25f);
         self.Depth = Depths.FormationSequences;
 
         Audio.Play("event:/sorbethelper/sfx/bubblereturn_split/start", self.level.Camera.Center);
+
         yield return 0.4f;
 
         Audio.Play("event:/sorbethelper/sfx/bubblereturn_split/slide", self.level.Camera.Center);
@@ -113,7 +131,7 @@ public class ReturnBubbleBehaviorController : Entity {
 
             // camera
             if (behavior.SmoothCamera) {
-                // based on non-bubble normal camera movement, but with a faster catchup speed (1 -> 10)
+                // based on normal camera movement, but with a faster catchup speed (1 -> 10)
                 Vector2 position = self.level.Camera.Position;
                 Vector2 cameraTarget = self.CameraTarget;
 
@@ -130,10 +148,12 @@ public class ReturnBubbleBehaviorController : Entity {
         self.Sprite.Scale = new Vector2(1.25f);
         // undo the sprite offset which is applied in CassetteFlyBegin. also checked in CassetteFlyEnd !!
         if (behavior.PlayerYOffsetApplied)
-            self.Sprite.Y -= 5f; behavior.PlayerYOffsetApplied = false;
+            self.Sprite.Y -= 5f;
+        behavior.PlayerYOffsetApplied = false;
         self.Sprite.Play("fallFast");
 
         Audio.Play("event:/sorbethelper/sfx/bubblereturn_split/pop", self.level.Camera.Center);
+
         yield return 0.2f;
 
         self.level.CanRetry = true;
@@ -154,7 +174,8 @@ public class ReturnBubbleBehaviorController : Entity {
 
         // in case the routine was maybe interrupted
         if (behavior.PlayerYOffsetApplied)
-            self.Sprite.Y -= 5f; behavior.PlayerYOffsetApplied = false;
+            self.Sprite.Y -= 5f;
+        behavior.PlayerYOffsetApplied = false;
 
         self.level.CanRetry = true;
         if (self.Depth == Depths.FormationSequences)
@@ -163,13 +184,9 @@ public class ReturnBubbleBehaviorController : Entity {
         self.level.FormationBackdrop.Alpha = 0.5f;
     }
 
-    // the vanilla bubble sounds are all in one single event, which means that in order for the speed of bubble to be adjusted it needs to be replaced with multiple split ones
-    // vanilla also does something funny though where instead of Player.StartCassetteFly playing the sound it's manually played by each entity that calls that
-    // so just to be safe im muting it at the source rather than case by case
-    // not checking the current "active" behavior since vanilla plays the sfx before updating the player's state
-    private static bool MuteVanillaSFX => GetReturnBubbleBehavior(Engine.Scene) is not null;
+    // need to mute the non-split vanilla bubble event here, since instead of Player.StartCassetteFly or something playing it, everywhere that calls it plays the sound manually ?? for some reason
     private static EventInstance On_Audio_CreateInstance(On.Celeste.Audio.orig_CreateInstance orig, string path, Vector2? position = null) {
-        if (path is SFX.game_gen_cassette_bubblereturn && MuteVanillaSFX)
+        if (path == SFX.game_gen_cassette_bubblereturn && GetReturnBubbleBehavior(Engine.Scene) is not null)
             return null;
 
         return orig(path, position);
@@ -177,67 +194,66 @@ public class ReturnBubbleBehaviorController : Entity {
 
     private static void On_OnSquish(On.Celeste.Player.orig_OnSquish orig, Player self, CollisionData data) {
         // only care about behavior if in cassettefly & skip onsquish if behavior exists and has disablesquish set
-        if (self.StateMachine.State is Player.StCassetteFly && GetActiveBehavior(self) is { CollisionMode: ReturnBubbleBehavior.CollisionModes.SquishFix })
+        if (self.StateMachine.State == Player.StCassetteFly && GetActiveBehavior(self) is { CollisionMode: ReturnBubbleBehavior.CollisionModes.SquishFix })
             return;
 
         orig(self, data);
     }
 
-    // absolutely awful but setting player.collidable to false wouldnt prevent the player itself from colliding with other entities, and i dont think theres a better way to do this??? or im stupid
-    // in any case like, i already did this for another mod once so if i ever wanted to port that mechanic here id have to do this anyway
-    // could also add hook lazy loading so any maps where this never does anything dont get any extra overhead
+    // this suckssss
     private static void IL_Collide_Check(ILContext il) {
         ILCursor cursor = new ILCursor(il);
 
         ILLabel endLabel = null;
 
-        if (!cursor.TryGotoNextBestFit(MoveType.After, i => i.MatchLdfld(out _), i => i.MatchBrfalse(out endLabel))) {
-            Logger.Error(LogID, $"failed to inject collision disabling in CIL code for {cursor.Method.Name}!");
+        if (!cursor.TryGotoNextBestFit(MoveType.After,
+            instr => instr.MatchLdfld(out _),
+            instr => instr.MatchBrfalse(out endLabel))) {
+            Logger.Error(LogID, $"failed to inject code to disable collision in return bubbles in CIL code for {cursor.Method.Name}!");
             return;
         }
 
-        Logger.Verbose(LogID, $"injecting collision disabling at {cursor.Index} in CIL code for {cursor.Method.Name}");
+        Logger.Verbose(LogID, $"injecting code to disable collision in return bubbles at {cursor.Index} in CIL code for {cursor.Method.Name}");
 
         cursor.EmitLdarg0();
         cursor.EmitLdarg1();
         cursor.EmitDelegate(ShouldDisableCollision);
         cursor.EmitBrtrue(endLabel);
-    }
 
-    private static bool ShouldDisableCollision(Entity a, Entity b) {
-        // looks a bit worse than it is maybe , code in both if checks is the same but i didnt want to include another method call and im not confident in using aggressiveinlining
-        // still awful though gladstare
-        if (a is Player p1) {
-            if (p1.StateMachine.State is not Player.StCassetteFly)
-                return false;
+        return;
 
-            ReturnBubbleBehavior behavior = GetActiveBehavior(p1);
-            if (behavior is null)
-                return false;
+        static bool ShouldDisableCollision(Entity a, Entity b) {
+            if (a is Player p1) {
+                if (p1.StateMachine.State != Player.StCassetteFly)
+                    return false;
 
-            if (behavior.CollisionMode == ReturnBubbleBehavior.CollisionModes.TriggersOnly)
-                return b is not Trigger;
+                ReturnBubbleBehavior behavior = GetActiveBehavior(p1);
+                if (behavior is null)
+                    return false;
 
-            return behavior.CollisionMode == ReturnBubbleBehavior.CollisionModes.NoCollide;
+                if (behavior.CollisionMode == ReturnBubbleBehavior.CollisionModes.TriggersOnly)
+                    return b is not Trigger;
+
+                return behavior.CollisionMode == ReturnBubbleBehavior.CollisionModes.NoCollide;
+            }
+
+            if (b is Player p2) {
+                if (p2.StateMachine.State != Player.StCassetteFly)
+                    return false;
+
+                ReturnBubbleBehavior behavior = GetActiveBehavior(p2);
+                if (behavior is null)
+                    return false;
+
+                if (behavior.CollisionMode == ReturnBubbleBehavior.CollisionModes.TriggersOnly)
+                    return a is not Trigger;
+
+                return behavior.CollisionMode == ReturnBubbleBehavior.CollisionModes.NoCollide;
+            }
+
+            return false;
         }
-
-        if (b is Player p2) {
-            if (p2.StateMachine.State is not Player.StCassetteFly)
-                return false;
-
-            ReturnBubbleBehavior behavior = GetActiveBehavior(p2);
-            if (behavior is null)
-                return false;
-
-            if (behavior.CollisionMode == ReturnBubbleBehavior.CollisionModes.TriggersOnly)
-                return a is not Trigger;
-
-            return behavior.CollisionMode == ReturnBubbleBehavior.CollisionModes.NoCollide;
-        }
-
-        return false;
     }
 
     #endregion
-
 }
